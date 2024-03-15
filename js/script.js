ce23a7f458270e378cf205facc3f3b5e1565aaed
Matteo0421 <matteo.carola99@gmail.com>
2024-03-15 11:49:30 +0100

    [end]milestone-3

--- js/script.js
+++ js/script.js
@@ -21,13 +21,21 @@ createApp({
     addMessage() {
       const orario = new Date().toLocaleString();
       
-      const index = 0; 
       this.contacts[this.activeContact].messages.push({ 
         date: orario, 
         message: this.newMessage, 
         status: 'sent' });
     
       this.newMessage = '';
+
+      setTimeout(() => {
+        const receivedMessage = {
+            date: new Date().toLocaleString(),
+            message: 'Ok',
+            status: 'received'
+        };
+        this.contacts[this.activeContact].messages.push(receivedMessage);
+    }, 1000);
   },
 
   changeActiveContact(index) {
